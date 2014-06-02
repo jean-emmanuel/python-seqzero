7ef32e7038be6ecca3d339f0ec77364101422243
orlammd <orl@ammd.net>
2014-06-02 22:29:50 +0200

    the same goes for scenes

--- main.sc.py
+++ main.sc.py
@@ -1,13 +1,12 @@
 from pyOSCseq import *
 from scenes import scenes_list
 
-seq = pyOSCseq(640,123451,'192.168.1.82:56418 192.168.1.82:7770 localhost:5555',scenes_list)
+# sequencer creation
+seq = pyOSCseq(640,123451,'192.168.0.112:56418 192.168.0.112:7770 localhost:5555',scenes_list)
 
 
+# sequences creation
 
-seq.addSequence('test',[
-    [':/Sequencer/Scene/Play', 'Intro_generique'],
-    [':/Sequencer/Sequence/Stop', 'test']
-])
 
+# Run
 seq.play()
