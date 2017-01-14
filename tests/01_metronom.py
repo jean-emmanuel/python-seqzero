4e418dccb75b321654d59c10c9d2143573ca4fc2
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-01-14 18:30:51 +0100

    rewrite test without pyo, only liblo

--- tests/01_metronom.py
+++ tests/01_metronom.py
@@ -4,18 +4,10 @@ import sys
 sys.path.append("..")
 
 from seqzero import Sequencer
-from pyo import Server, Metro, Trig, Timer, Change, OscDataReceive, TrigFunc
-
+from liblo import ServerThread
+from time import time
 
 bpm = 120
-
-s = Server(audio="jack")
-s.boot()
-s.start()
-
-
-# Metronom
-metronom = Metro(60. / bpm)
 
 # Sequencer
 seq = Sequencer(bpm=bpm, port=12345, target='localhost:9900')
@@ -24,28 +16,20 @@ seq.addSequence('metronom',[
     ['/tic'],
 ])
 
-# Sequencer's monitor
-t = False
-sequencer_trig = Trig()
-def tic(a):
-    sequencer_trig.play()
-    # launch the metronom at first msg
-    global t
-    if not t:
-        metronom.play()
-
-sequencer_monitor = OscDataReceive(9900, "/tic", tic)
-
-
-# Compute & print difference between the two
-timer = Timer(sequencer_trig, metronom)
-
+t = time()
 def print_diff():
-    # print abs(timer.get() - 0.25) - 0.25
-    print(timer.get())
+    global t
+    l = str(round(10000 * (60. / seq.bpm - (time() - t))) / 10) + 'ms'
+    l = ' ' + l if l[0] != '-' else l
+    print('latency: ' + l)
 
-change = Change(timer)
-printer = TrigFunc(change, print_diff)
+    t = time()
+
+sequencer_monitor = ServerThread(port=9900)
+sequencer_monitor.add_method('/tic', None, print_diff)
+sequencer_monitor.start()
+
+
 
 
 seq.send(':/Sequencer/Sequence/Enable', 'metronom')
