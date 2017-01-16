cb614086216eff1666766c3af9b71681c63691d6
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-01-16 16:04:06 +0100

    reduce  attack len

--- tests/02_metronom.py
+++ tests/02_metronom.py
@@ -19,7 +19,7 @@ seq.sequence_add('metronom',[
 def tic(*a):
     env.play()
 osc = OscDataReceive(9900, '/tic', tic)
-env = Adsr(attack=0.01, decay=0.1, sustain=0.1, release=0.1, dur=0.2, mul=0.5)
+env = Adsr(attack=0.005, decay=0.1, sustain=0.05, release=0.05, dur=0.2, mul=0.5)
 sin = Sine(1500, mul=env).mix(2).out()
 
 seq.send(':/Sequencer/Sequence/Enable', 'metronom')
