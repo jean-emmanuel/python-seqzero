5da33b7880abe14cbfd2070f7cfb0e73fccb6164
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-02-07 15:03:47 +0100

    tests: add network latency measurement

---
+++ tests/03_network.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+import sys
+sys.path.append("../..")
+
+from seqzero import Sequencer
+from liblo import send
+from time import time, sleep
+
+# Sequencer
+
+
+
+class Scenes():
+
+    def test(self, seq, timer):
+
+        print 'Network + Code latency = %f ms' % ((time() - timer.clock)*1000)
+        timer.wait(1, 'beat')
+        seq.send('127.0.0.1:12345', '/Sequencer/Scene/Play', 'test', 't:%f' % time())
+
+seq = Sequencer(bpm=240, port=12345, target='localhost:9900', scenes=Scenes())
+seq.scene_play('test')
+seq.play()
+seq.start()
