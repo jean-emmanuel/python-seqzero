5d2d51a1a66e26f2654493365b500b3ac08ac251
Wladimir Ohrelianov II <woii@SCstage.sebkhachott.net>
2017-02-12 11:19:30 +0100

    osc: fix permissive osc send

--- osc.py
+++ osc.py
@@ -53,6 +53,8 @@ class Server(ServerThread):
         if 'osc.udp://' not in target:
             target = 'osc.udp://' + target
 
+        print target
+
         ServerThread.send(self, target, *message)
 
     def route_osc(self, address, *args):
